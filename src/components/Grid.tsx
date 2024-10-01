e2b4993e2cdd36ee136787766343c4239c664314
Nik <laveosa@yahoo.com>
2024-10-01 12:55:37 +0300

    Add "toggle todo" and "remove" handlers logic

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,15 +1,57 @@
-import GridItem from './GridItem.tsx';
+import { useState } from 'react';
 
-const initialTodos = ['buy groceries', 'walk the dog', 'do laundry'];
+import GridItem from './GridItem.tsx';
+import { ITodo } from '../consts/interfaces/ITodo.ts';
+
+const initialTodos: ITodo[] = [
+  {
+    id: 1,
+    text: 'buy groceries',
+  },
+  {
+    id: 2,
+    text: 'walk the dog',
+  },
+  {
+    id: 3,
+    text: 'do laundry',
+    isCompleted: true,
+  },
+  {
+    id: 4,
+    text: 'exercise',
+  },
+];
 
 function Grid() {
+  const [todoList, setTodoList] = useState<ITodo[]>(initialTodos);
+
+  function onToggleTodo(todoId: number) {
+    setTodoList((todoList: ITodo[]) => {
+      return todoList.map((item: ITodo) => {
+        if (item.id === todoId) {
+          item.isCompleted = !item.isCompleted;
+        }
+        return item;
+      });
+    });
+  }
+
+  function onRemoveTodo(todoId: number) {
+    setTodoList(todoList.filter((item: ITodo) => item.id !== todoId));
+  }
+
   return (
     <div>
-      <div>
-        {initialTodos.map((todo: string) => (
-          <GridItem>{todo}</GridItem>
-        ))}
-      </div>
+      {todoList.map((todo: ITodo) => (
+        <div key={todo.id}>
+          <GridItem
+            {...todo}
+            onToggleTodo={onToggleTodo}
+            onRemoveTodo={onRemoveTodo}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,10 +1,20 @@
-function GridItem({ className, children }) {
+function GridItem({
+  className,
+  id,
+  text,
+  isCompleted,
+  onToggleTodo,
+  onRemoveTodo,
+}) {
   return (
     <div
       className={`flex h-[50px] cursor-pointer items-center justify-between border-b border-black/[8%] px-8 text-[14px] ${className}`}
+      onClick={() => onToggleTodo(id)}
     >
-      <span>{children}</span>
-      <button>❌</button>
+      <span className={isCompleted ? 'text-[#ccc] line-through' : ''}>
+        {text}
+      </span>
+      <button onClick={() => onRemoveTodo(id)}>❌</button>
     </div>
   );
 }
