8c76aa36d45868d2bdfeacaf352c907096aa5135
Nik <laveosa@yahoo.com>
2024-10-03 16:21:39 +0300

    Update authentication

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,8 @@ createRoot(document.getElementById('root')!).render(
   <KindeProvider
     clientId="c2573105650447bb936aa39b95465a9c"
     domain="https://nikdev.kinde.com"
-    redirectUri="http://localhost:5173"
-    logoutUri="http://localhost:5173"
+    redirectUri="https://byte-grad-todo.vercel.app"
+    logoutUri="https://byte-grad-todo.vercel.app"
   >
     <AuthContextProvider>
       <TodoListContextProvider>
