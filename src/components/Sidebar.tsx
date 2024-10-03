0ec3d862bce4503077ab58fe098ab9442c006cd0
Nik <laveosa@yahoo.com>
2024-10-03 11:58:38 +0300

    Add auth loading view

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,8 @@ import AddTodoForm from './AddTodoForm.tsx';
 import Button from './Button.tsx';
 
 function Sidebar() {
-  const { login, logout, register, isAuthenticated, user } = useKindeAuth();
+  const { login, logout, register, isAuthenticated, user, isLoading } =
+    useKindeAuth();
 
   function onLogin() {
     login().then((res) => {
@@ -27,25 +28,34 @@ function Sidebar() {
   return (
     <section className="col-[2/3] row-[2/3] flex flex-col border-l border-black/[0.04] bg-[#fffcf9] px-[25px] pb-[28px] pt-[18px]">
       <AddTodoForm />
-      <div className="mt-auto space-y-2">
-        {isAuthenticated && (
-          <div>
-            <p className="text-gray-500 text-sm mb-2">
-              Welcome:{' '}
-              <span className="font-bold">
-                {user.given_name} {user.family_name}
-              </span>
-            </p>
-            <Button onClick={onLogout}>Log out</Button>
-          </div>
-        )}
-        {!isAuthenticated && (
-          <>
-            <Button onClick={onLogin}>Log in</Button>
-            <Button onClick={onRegister}>Register</Button>
-          </>
-        )}
-      </div>
+      {isLoading && (
+        <div className="my-auto space-y-2">
+          <p className="text-center text-gray-300 text-4xl mb-2 font-bold">
+            loading...
+          </p>
+        </div>
+      )}
+      {!isLoading && (
+        <div className="mt-auto space-y-2">
+          {isAuthenticated && (
+            <div>
+              <p className="text-gray-500 text-sm mb-2">
+                Welcome:{' '}
+                <span className="font-bold">
+                  {user?.email ? user?.email : user?.given_name}
+                </span>
+              </p>
+              <Button onClick={onLogout}>Log out</Button>
+            </div>
+          )}
+          {!isAuthenticated && (
+            <>
+              <Button onClick={onLogin}>Log in</Button>
+              <Button onClick={onRegister}>Register</Button>
+            </>
+          )}
+        </div>
+      )}
     </section>
   );
 }
